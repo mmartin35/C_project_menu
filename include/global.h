2513d8ec11d8e67d710900562af44d9a2e81fbda
user35p74a <matthieu1.martin@epitech.eu>
2023-01-18 17:17:02 +0100

    > updating:   Makefile   README.md--  src/master.c   terminal_ui |  > comment:  |  > added: include/global.h |

---
+++ include/global.h
@@ -0,0 +1,19 @@
+/*
+** Language        : C
+** Title           : global.h
+** File description:
+** <------------------------------------------->
+** Made by         : matthieu1.martin@epitech.eu
+** Creation date   : 18/01/2023 at 12:39:22
+*/
+#pragma once
+#ifndef GLOBAL_H_
+    #define GLOBAL_H_
+
+    #include <ncurses.h>
+    #include <stdlib.h>
+
+    void setup_options(int i);
+
+
+#endif
